7f74e49894c771dbffc7e544dfbf9b51f56fdb0c
tendalamaw <tendalamaw@gmail.com>
2025-02-23 10:31:49 +0300

    Enabled Vercel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { NavigationTabs } from "./components/navigation-tabs";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
             {children}
           </div>
         </div>
+        <Analytics />
       </body>
     </html>
   );
